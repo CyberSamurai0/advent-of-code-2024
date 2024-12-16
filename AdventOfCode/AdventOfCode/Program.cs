26f890513626dfd10c584ca0ace9d374f9dd6637
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2024-12-15 21:37:15 -0500

    Update Program.cs

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -1,22 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using AdventOfCode.challenges;
 
-string day;
-string input;
+string day = args.Length >= 1 ? args[0] : "1";
 
-if (args.Length >= 2) {
-	day = args[0];
-	input = args[1];
-} else if (args.Length == 1) {
-	day = "1";
-	input = args[1];
-} else {
-	Console.WriteLine("Missing required input. Usage:");
-	Console.WriteLine(" [day=1] <inputTextFile>");
-	return 1;
-}
-
-if (day.Equals("1")) new HistorianHysteria(input).Run();
-
+// WARNING - you must configure the working directory to the source code directory so that it knows where to retrieve input files!
+if (day.Equals("1")) new HistorianHysteria("./inputs/one.txt").Run();
 return 0;
