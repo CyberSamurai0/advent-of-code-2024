ef7196c96ce643005e842521ff6f8a24c727fccb
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2024-12-15 21:37:13 -0500

    Solve HistorianHysteria Part 1

--- AdventOfCode/AdventOfCode/challenges/HistorianHysteria.cs
+++ AdventOfCode/AdventOfCode/challenges/HistorianHysteria.cs
@@ -1,11 +1,33 @@
+using System.Runtime.CompilerServices;
+
 namespace AdventOfCode.challenges;
 
-public class HistorianHysteria : IChallenge {
-	public HistorianHysteria(string inFile) {
-		
-	}
+public class HistorianHysteria : Challenge {
+	public HistorianHysteria(string inFile) : base(inFile) { }
+
+	private List<int>? _left;
+	private List<int>? _right;
+
+	private int _sum;
 	
 	public void Run() {
+		_left = new List<int>();
+		_right = new List<int>();
+		_sum = 0;
 		
+		foreach (string l in this.Input.Lines) {
+			string[] x = l.Split("   ");
+			_left.Add(int.Parse(x[0]));
+			_right.Add(int.Parse(x[1]));
+		}
+		
+		_left.Sort();
+		_right.Sort();
+
+		for (int i = 0; i < this.Input.Lines.Length; i++) {
+			_sum += Math.Abs(_left[i] - _right[i]);
+		}
+		
+		Console.WriteLine("Total Distance between Lists: " + _sum);
 	}
 }
