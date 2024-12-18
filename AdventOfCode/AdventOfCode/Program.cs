b2342a9d9d41721e48656062309ac6d142fde814
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2024-12-17 20:46:52 -0500

    Add days 3 and 4

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -9,4 +9,6 @@ string day = args.Length >= 1 ? args[0] : "1";
 // WARNING - you must configure the working directory to the source code directory so that it knows where to retrieve input files!
 if (day.Equals("1")) new HistorianHysteria("./inputs/one.txt").Run();
 if (day.Equals("2")) new RedNosedReports("./inputs/two.txt").Run();
+if (day.Equals("3")) new MullItOver("./inputs/three.txt").Run();
+if (day.Equals("4")) new CeresSearch("./inputs/four.txt").Run();
 return 0;
